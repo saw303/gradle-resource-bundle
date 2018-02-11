ebbf1912b88f92a59c51bb6cc26f6b8cc9e9a3ea
Silvio Wangler <silvio.wangler@gmail.com>
2018-02-11 22:58:23 +0100

    Upgraded licences

--- gradle-plugin/src/main/java/ch/silviowangler/gradle/i18n/ResourceBundlePlugin.java
+++ gradle-plugin/src/main/java/ch/silviowangler/gradle/i18n/ResourceBundlePlugin.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2016 - 2017 Silvio Wangler (silvio.wangler@gmail.com)
+ * Copyright 2016 - 2018 Silvio Wangler (silvio.wangler@gmail.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
